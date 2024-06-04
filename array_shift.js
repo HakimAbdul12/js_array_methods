d1aec65c7eb67074a663c190d293c3d046a48ed1
HakimAbdul12 <abdulhakimaben@gmail.com>
2024-06-04 01:03:19 +0000

    shift method

---
+++ array_shift.js
@@ -0,0 +1,5 @@
+//this method shifts off the first element of an array and returns the shifted element
+const array = ["foo", "bar", "baz"];
+const shifted = array.shift();
+console.log(shifted); // foo
+console.log(array); // ["bar", "baz"]
